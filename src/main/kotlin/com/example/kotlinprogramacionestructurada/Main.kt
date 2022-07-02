e420d467aa105b4496f4c2e894f0631fb9786a77
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-02 02:25:34 -0500

    Ejercicio 10:indique cual es el mayor de cuatro numeros enteros

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -1,5 +1,12 @@
 package com.example.kotlinprogramacionestructurada
 
 fun main() {
-    println("${calcularSuma(5,6)}")
+    println("Programa que indique cual es el mayor de cuatro numeros enteros ")
+    println("Ingrese 4 numeros:")
+    val primerNumero= readLine()!!.toInt()
+    val segundoNumero= readLine()!!.toInt()
+    val tercerNumero= readLine()!!.toInt()
+    val cuartoNumero= readLine()!!.toInt()
+
+    calcularElMayor(primerNumero,segundoNumero,tercerNumero,cuartoNumero)
 }
--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
@@ -1,6 +1,17 @@
 package com.example.kotlinprogramacionestructurada
 
-fun calcularSuma(a: Int, b: Int): Int {
-    val suma = a + b
-    return suma
+fun calcularElMayor(primernumero: Int, segundoNumero: Int, tercerNumero: Int, cuartoNumero: Int) {
+    if (primernumero > segundoNumero && primernumero > tercerNumero && primernumero > cuartoNumero) {
+        println("El mayor de $primernumero,$segundoNumero,$tercerNumero,$cuartoNumero es: $primernumero")
+    }
+    if (segundoNumero > primernumero && segundoNumero > tercerNumero && segundoNumero > cuartoNumero) {
+        println("El mayor de $primernumero,$segundoNumero,$tercerNumero,$cuartoNumero es: $segundoNumero")
+    }
+    if (tercerNumero > primernumero && tercerNumero > segundoNumero && tercerNumero > cuartoNumero) {
+        println("El mayor de $primernumero,$segundoNumero,$tercerNumero,$cuartoNumero es: $tercerNumero")
+
+    }
+    if (cuartoNumero > primernumero && cuartoNumero > segundoNumero && cuartoNumero > tercerNumero) {
+        println("El mayor de $primernumero,$segundoNumero,$tercerNumero,$cuartoNumero es: $cuartoNumero")
+    }
 }
