52fb845ae9babf4dd6322f4daaabc8ab0c91ae17
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-02 10:07:51 -0500

    Ejercicio 23:area de un cuadrado, un rectangulo y triangulo

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -1,5 +1,15 @@
 package com.example.kotlinprogramacionestructurada
 
 fun main() {
-    println("${calcularSuma(5,6)}")
+    print("**** Programa que calcule el area de un cuadrado, un rectangulo y triangulo ****")
+
+    println(" --------------------------- ")
+    println("|           MENU            |")
+    println("|     1)Area Cuadrado       |")
+    println("|     2)Area Rectangulo     |")
+    println("|     3)Area Triangulo      |")
+    println("|___________________________|")
+    println("ingrese numero: 1-3")
+    val opcion = readLine()!!.toInt()
+    areaDeFiguras(opcion)
 }
--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
@@ -1,6 +1,33 @@
 package com.example.kotlinprogramacionestructurada
 
-fun calcularSuma(a: Int, b: Int): Int {
-    val suma = a + b
-    return suma
+fun areaDeFiguras(opcion:Int){
+    when (opcion) {
+        1 -> {
+            println("CUADRADO")
+            println("DAME LA MEDIDA DE UN LADO")
+            val medida = readLine()!!.toInt()
+            val A = medida * medida
+            println("Area es igual a: $A")
+        }
+        2 -> {
+            println("RECTANGULO")
+            println("DAME LAS MEDIDAS DE SUS LADOS:")
+            val primerLado = readLine()!!.toInt()
+            val segundoLado = readLine()!!.toInt()
+            val A = primerLado * segundoLado
+            println("Area es: $A")
+        }
+        3 -> {
+            println("TRIANGULO")
+            println("DAME BASE Y ALTURA:")
+            val base = readLine()!!.toInt()
+            val altura = readLine()!!.toInt()
+            val A = (base * altura) / 2
+            println("Area es: $A")
+
+        }
+        else -> {
+            println("FIN DEL PROGRAMA")
+        }
+    }
 }
