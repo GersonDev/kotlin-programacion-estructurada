ccdb99a9c015118665c1e1a39ca8089f184bd8c5
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-02 03:21:49 -0500

    Ejercicio 14:Indicar si la suma de dos valores es positiva, negativa o cero

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -1,5 +1,10 @@
 package com.example.kotlinprogramacionestructurada
 
-fun main() {
-    println("${calcularSuma(5,6)}")
+fun main(args: Array<String>) {
+    println("Indicar si la suma de dos valores es positiva, negativa o cero")
+    println("Ingrese primer numero: ")
+    val primerNumero = readLine()!!.toInt()
+    println("Ingrese segundo numero:")
+    val segundoNumero = readLine()!!.toInt()
+    imprimirSiEsPositivoONegativo(primerNumero, segundoNumero)
 }
--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
@@ -1,6 +1,21 @@
 package com.example.kotlinprogramacionestructurada
 
-fun calcularSuma(a: Int, b: Int): Int {
-    val suma = a + b
+ fun sumaDeDosNumeros(primerNumero: Int, segundoNumero: Int): Int {
+    val suma = primerNumero + segundoNumero
     return suma
+}
+
+fun imprimirSiEsPositivoONegativo(primerNumero: Int, segundoNumero: Int){
+    val suma = sumaDeDosNumeros(primerNumero, segundoNumero)
+    when {
+        suma > 0 -> {
+            print("$suma es positivo")
+        }
+        suma < 0 -> {
+            print("$suma es negativo")
+        }
+        else -> {
+            print("el $suma  es un numero Neutro")
+        }
+    }
 }
