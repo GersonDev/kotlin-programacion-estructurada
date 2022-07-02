76f3796d242e40107e28b6946aafba9bd8b693f2
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-02 12:00:20 -0500

    Ejercicio 28:programa que verifica si un dato ingresado por teclado corresponde a la contraseña 252525

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -1,5 +1,11 @@
 package com.example.kotlinprogramacionestructurada
 
 fun main() {
-    println("${calcularSuma(5,6)}")
+    do {
+        println("programa que verifica si un dato ingresado por teclado corresponde a la contraseña 252525")
+        println("DAME LA CONTRASEÑA")
+        val numeroIngresado = readLine()!!.toInt()
+        if (numeroIngresado != 252525) println("CONTRASEÑA INCORRECTA")
+    } while (numeroIngresado != 252525)
+    println("CONTRASEÑA CORRECTA ")
 }
