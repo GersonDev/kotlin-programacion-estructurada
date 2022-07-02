140dcd36629bbf2f435f047fb6e51dde2941a89b
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-02 11:42:16 -0500

    Ejercicio 25:indique el numero de digitos que tiene una cantidad a la izquierda del punto decimal

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -3,6 +3,11 @@ package com.example.kotlinprogramacionestructurada
 fun main() {
     println("**** Programa que indique el numero de digitos que tiene una cantidad a la izquierda del punto decimal ****")
     println("Ingrese numero: ")
-    var numero = readLine()!!.toDouble()
-    numerosIzquierda(numero)
+    var x = readLine()!!.toDouble()
+    var c = 0
+    do {
+        x /= 10
+        c += 1
+    } while (x > 1)
+    println("Digitos a la izquierda:$c")
 }
--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
@@ -1,10 +1,2 @@
 package com.example.kotlinprogramacionestructurada
 
-fun numerosIzquierda(numero: Double) {
-    var c: Double = 0.0
-    do {
-        var numero = numero / 10
-        c = c + 1
-    } while (numero >= 1)
-    print("Los digitos de la izquierda son: $c)")
-}
