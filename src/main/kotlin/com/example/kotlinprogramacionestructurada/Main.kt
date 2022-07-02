924e57e6afa229cf518c8d85f8dfb6ae75da290b
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-02 11:06:12 -0500

    Ejercicio 25:cantidad de numeros a la izquierda del punto decimal

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -1,5 +1,8 @@
 package com.example.kotlinprogramacionestructurada
 
 fun main() {
-    println("${calcularSuma(5,6)}")
+    println("**** Programa que indique el numero de digitos que tiene una cantidad a la izquierda del punto decimal ****")
+    println("Ingrese numero: ")
+    var numero = readLine()!!.toDouble()
+    numerosIzquierda(numero)
 }
--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
@@ -1,6 +1,10 @@
 package com.example.kotlinprogramacionestructurada
 
-fun calcularSuma(a: Int, b: Int): Int {
-    val suma = a + b
-    return suma
+fun numerosIzquierda(numero: Double) {
+    var c: Double = 0.0
+    do {
+        var numero = numero / 10
+        c = c + 1
+    } while (numero >= 1)
+    print("Los digitos de la izquierda son: $c)")
 }
