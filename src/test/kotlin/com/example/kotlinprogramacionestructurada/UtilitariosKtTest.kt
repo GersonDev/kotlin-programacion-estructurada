8898552ea4cdd2124e53ccf33d5aa888d7487f34
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-03 07:38:10 -0500

    Ejercicio 3:Programa para calcular el area el perimetro del pentagono

--- src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
+++ src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
@@ -6,12 +6,12 @@ import org.junit.Assert.*
 class UtilitariosKtTest {
 
     @Test
-    fun `dado el ingreso del apotema y lado debe calcularse el area  del pentagono`() {
+    fun `dado el ingreso del apotema que tiene como valor 5 y lado que tiene como valor 6 debe calcularse el area  del pentagono`() {
         val area = calcularAreaPentagono(5, 6)
         assertTrue(area == 15)
     }
     @Test
-    fun `dado el ingreso de el lado debe calcularse el perimetro del pentagono`() {
+    fun `dado el ingreso de el lado que tiene como valor 5 debe calcularse el perimetro del pentagono`() {
         val perimetro = calcularPerimetroPentagono(5)
         assertTrue(perimetro == 25)
     }
