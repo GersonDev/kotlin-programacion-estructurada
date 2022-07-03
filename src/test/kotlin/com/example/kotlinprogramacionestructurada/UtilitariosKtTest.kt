56178aa3c2e9792abd3c904bbb44d64bfe5bc5b2
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-03 07:41:09 -0500

    Ejercicio 5:Programa que calcule el cuadrado de 243

--- src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
+++ src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
@@ -6,7 +6,7 @@ import org.junit.Assert.*
 class UtilitariosKtTest {
 
     @Test
-    fun `dar como resultado de 243 al cuadrado`() {
+    fun `dado  ingresa el numero 243 obtener su cuadrado`() {
         val cuadrado = CalcularCuadrado()
         assertTrue(cuadrado == 59049)
     }
