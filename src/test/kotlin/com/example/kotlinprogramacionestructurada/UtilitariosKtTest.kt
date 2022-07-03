8e74ca2a9fca18963978974bc5c8301f869c08de
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-03 10:10:11 -0500

    Ejercicio 22:programa que realize operaciones aritmeticas

--- src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
+++ src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
@@ -5,9 +5,5 @@ import org.junit.Assert.*
 
 class UtilitariosKtTest {
 
-    @Test
-    fun `operaciones aricteticas de 2 numeros`() {
-        val suma = calcularOperacionAritmeticas(1, 6,6)
 
-    }
 }
