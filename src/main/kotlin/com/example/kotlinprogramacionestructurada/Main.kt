69989239b8dc366d4b4ac577f356576f515b1570
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-03 10:13:36 -0500

    Ejercicio 24:Escribir un programa que calcule el más grande, el más pequeño y la media de N números.

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -1,6 +1,7 @@
 package com.example.kotlinprogramacionestructurada
 
 fun main() {
+    println("calcule el más grande, el más pequeño y la media de N números.")
     println("Ingrese la cantidad de numeros: ")
     var cantidadDeNumeros = readLine()!!.toInt()
     calcularGrandePequeñoMedio(cantidadDeNumeros)
