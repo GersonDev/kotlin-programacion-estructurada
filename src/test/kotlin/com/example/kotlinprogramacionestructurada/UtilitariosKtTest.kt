8c068bca8f4e490fce0e9f411f1c5c22808952ed
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-03 07:44:41 -0500

    Ejercicio 8:Programa para calcular la hipotenusa de un triangulo rectangulo

--- src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
+++ src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
@@ -6,7 +6,7 @@ import org.junit.Assert.*
 class UtilitariosKtTest {
 
     @Test
-    fun `dado los 2 numeros ingresado calcular la hipotenusa de un triangulo rentangulo`() {
+    fun `dado los 2 numeros ingresado el 5 y 6 calcular la hipotenusa de un triangulo rentangulo`() {
         val hipotanusa = calcularHipotenusa(5, 6)
         assertTrue(hipotanusa == 12)
     }
