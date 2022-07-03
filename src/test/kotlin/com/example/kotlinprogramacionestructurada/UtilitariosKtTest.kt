6991921ff2dd4f355038080673723abb52e7efb2
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-03 09:47:14 -0500

    Ejercicio 17:Programa que calcule el cambio del cliente

--- src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
+++ src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
@@ -6,8 +6,13 @@ import org.junit.Assert.*
 class UtilitariosKtTest {
 
     @Test
-    fun `dado los numeros 5 y 6 entonces la suma debe ser 11`():Unit {
-        val cambioDeCliente= calcularCambio(100, 500)
-        assertTrue(cambioDeCliente == 400)
+    fun `dado el ingreso del monto que es 1000 y el costo que es 500 dar el resultado `():Unit {
+        val vueltoDelCliente= calcularCambio(1000, 500)
+        assertTrue(vueltoDelCliente == 500)
     }
+    @Test
+    fun `dado el ingreso del monto que es 100 y el costo que es 500 dar el resultado `():Unit {
+    val faltanteDelCliente= calcularCambio(100, 500)
+    assertTrue(faltanteDelCliente == 400)
+}
 }
