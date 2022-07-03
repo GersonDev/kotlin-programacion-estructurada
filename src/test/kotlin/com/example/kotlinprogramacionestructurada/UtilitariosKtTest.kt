259d73d9bf9708b2b32c1c0ad6188f75572ca032
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-03 09:53:56 -0500

    Ejercicio 20:Programa pàra saber el angulo

--- src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
+++ src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
@@ -5,9 +5,5 @@ import org.junit.Assert.*
 
 class UtilitariosKtTest {
 
-    @Test
-    fun `dado los numeros 5 y 6 entonces la suma debe ser 11`() {
-        val angulo = calcularAngulo(-10)
-        assertTrue(angulo)
-    }
+
 }
