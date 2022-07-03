d9723e8b9f3dc60c12b9b20c4c53aad5a04ffe35
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-03 07:42:23 -0500

    Ejercicio 6:Programa que calcule el perimetro y area del rectangulo

--- src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
+++ src/test/kotlin/com/example/kotlinprogramacionestructurada/UtilitariosKtTest.kt
@@ -6,12 +6,12 @@ import org.junit.Assert.*
 class UtilitariosKtTest {
 
     @Test
-    fun `dado la base y altura calcular el perimetro`() {
+    fun `dado la base con valor de 5 y altura con valor de 6 calcular el perimetro`() {
         val perimetro = calcularPerimetro(5, 6)
         assertTrue(perimetro == 22)
     }
     @Test
-    fun `dado la base y altura calcular el area`() {
+    fun `dado la base con valor de 5 y altura con valor de 6 calcular el area`() {
         val area = calcularArea(5, 6)
         assertTrue(area == 30)
     }
