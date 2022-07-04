63967096d952e68d025a3c751600e48bbaff7df8
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-04 10:16:26 -0500

    Ejercicio 38:programa que capture con número enteros una matriz de 10 X 10 y al final de cada renglón imprima la suma de sus elementos

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinprogramacionestructurada
 
 fun main() {
-    println("${calcularSuma(5,6)}")
+    println("programa que capture con número enteros una matriz de 10 X 10 y al final de cada renglón imprima la suma de sus elementos")
+    calcularSumaDeUnVectorPorFila()
 }
--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
@@ -1,6 +1,35 @@
 package com.example.kotlinprogramacionestructurada
-
-fun calcularSuma(a: Int, b: Int): Int {
-    val suma = a + b
-    return suma
+fun calcularSumaDeUnVectorPorFila(){
+var s = 0
+var i = 1
+var j = 2
+val matriz = arrayOf(
+    intArrayOf(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1),
+    intArrayOf(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1),
+    intArrayOf(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1),
+    intArrayOf(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1),
+    intArrayOf(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1),
+    intArrayOf(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1),
+    intArrayOf(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1),
+    intArrayOf(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1),
+    intArrayOf(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1),
+    intArrayOf(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
+)
+for (i in i..10) {
+    for (j in 1..10) {
+        println("dame dato,$i,$j")
+        matriz[i][j] = readLine()!!.toInt()
+    }
+}
+for (i in i..10) {
+    s = 0
+    for (j in 1..10) {
+        s += matriz[i][j]
+    }
+    for (j in j..10) {
+        println(matriz[i][j])
+    }
+    println("la suma de este reglon es:, $s")
+    println("SALTO DE LINEA")
+}
 }
