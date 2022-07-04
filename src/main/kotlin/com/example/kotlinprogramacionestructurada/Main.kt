aa73bd266d3df220257e2c7260a0b06d1e799f3f
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-04 10:58:39 -0500

    Ejercicio 39:Escribir un programa que capture la producción de 2 plantas por siete días. Indique qué planta produjo más y qué día.

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -1,6 +1,6 @@
 package com.example.kotlinprogramacionestructurada
 
 fun main() {
-    println("Programa que dice que planta produce mas")
-    calcularProduccion()
+    println("Escribir un programa que coloque datos en la diagonal principal de una matriz de 3 X 3")
+    calcularDiagonal()
 }
--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
@@ -1,35 +1,15 @@
 package com.example.kotlinprogramacionestructurada
-
-fun calcularProduccion() {
-    var a = 0
-    var b = 0
+fun calcularDiagonal() {
     val matriz = arrayOf(
-        intArrayOf(-1, -1),
-        intArrayOf(-1, -1),
-        intArrayOf(-1, -1),
-        intArrayOf(-1, -1),
-        intArrayOf(-1, -1),
-        intArrayOf(-1, -1),
-        intArrayOf(-1, -1)
+        intArrayOf(-1, -1, -1),
+        intArrayOf(-1, -1, -1),
+        intArrayOf(-1, -1, -1)
     )
     for (i in 0..2) {
-        for (j in 0..7) {
-            if (i == 1) {
-                println("dame produccion del dia:,$j de la planta 1")
-                matriz[i][j] = readLine()!!.toInt()
-                a += matriz[i][j]
-            }
-            if (i == 2) {
-                println("dame produccion del dia:,$j de la planta 2")
-                matriz[i][j] = readLine()!!.toInt()
-                b += matriz[i][j]
-            }
-        }
+        println("Dame dato")
+        matriz[i][i] = readLine()!!.toInt()
     }
-    if (a > b) {
-        println("la primera planta tuvo mayor produccion")
-    }
-    if (b > a) {
-        println("a segunda planta tuvo mayor produccion")
+    for (i in 0..2) {
+        println(matriz[i][i])
     }
 }
