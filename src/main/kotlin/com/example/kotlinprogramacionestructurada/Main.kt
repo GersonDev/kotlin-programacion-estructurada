eaad0b481bc5998f9aa977ebeaa1237a36618503
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-04 09:43:25 -0500

    Ejercicio 36:Escribir un programa que guarde la presión de 30 personas por tres días e indique día que la persona tuvo mayor presión, menor presión.

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -2,45 +2,5 @@ package com.example.kotlinprogramacionestructurada
 
 fun main() {
     println("programa que guarde la presión de 30 personas por tres días e indique día que la persona tuvo mayor presión, menor presión.")
-    var a = 0
-    var ma = 0
-    var x = 0
-    var y = 0
-    var me = 0
-    var h = 0
-    var k = 0
-    val filas = 3
-    val columnas = 3
-    val matriz = arrayOf(
-        intArrayOf(-1, -1, -1),
-        intArrayOf(-1, -1, -1),
-        intArrayOf(-1, -1, -1)
-    )
-    for (i in 0..filas - 1) {
-        for (j in 0..columnas - 1) {
-            println("Dame tu presion del dia:$j del mes:$i")
-            matriz[i][j] = readLine()!!.toInt()
-            a += matriz[i][j]
-            if (i == 1 && j == 1) {
-                ma = matriz[i][j]
-                x = i
-                y = j
-                me = matriz[i][j]
-                h = i
-                k = j
-            }
-            if (matriz[i][j] > ma) {
-                ma = matriz[i][j]
-                x = i
-                y = j
-            }
-            if (matriz[i][j] < me) {
-                me = matriz[i][j]
-                h = i
-                k = j
-            }
-        }
-    }
-    println("Dia en el que tuvo mas presion:$y,en el mes:$x")
-    println("Dia en el que menor presion:$k,en el mes:$h")
+   calcularPresionMayorYMenor()
 }
