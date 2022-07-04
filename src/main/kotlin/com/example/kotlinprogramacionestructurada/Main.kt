9ad9413aec008a5ea1fb39a8a5789d2c73966a19
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-04 09:43:47 -0500

    Ejercicio 37:Escribir un programa que guarde la presión de 30 personas por tres días e indique día que la persona tuvo mayor presión, menor presión.

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -1,6 +1,6 @@
 package com.example.kotlinprogramacionestructurada
 
 fun main() {
-    println("programa que guarde la presión de 30 personas por tres días e indique día que la persona tuvo mayor presión, menor presión.")
+    println("programa que guarde la presión de 30 personas por tres días e indique día que la persona tuvo mayor presión, menor presión:")
    calcularPresionMayorYMenor()
 }
