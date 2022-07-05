359d8d1a421b6d2d4e46a93033bfcdf5e7085605
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-05 06:31:19 -0500

    Ejercicio 44:Escribir un programa que lea un vector A de 10 elementos que determine e emprima cuál es el mayor y en qué posición se encuentra y cuál es el              menor y en qué posición se encuentra.

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Main.kt
@@ -1,5 +1,34 @@
 package com.example.kotlinprogramacionestructurada
 
 fun main() {
-    println("${calcularSuma(5,6)}")
-}
+    var i = 2
+    var ma = 1
+    var me = 0
+    var p1 = 0
+    var p2 = 0
+    val array: Array<Int> = arrayOf(10)
+    println("Dame los datos:")
+    array[i] = readLine()!!.toInt()
+    me = array[i]
+    val pe = 1
+    do {
+        array[i] = readLine()!!.toInt()
+        if (array[i] > ma) {
+            ma = array[i]
+            p1 = 1
+        }
+        if (array[i] < me) {
+            me = array[i]
+            p2 = i
+        }
+        i = i + 1
+        while (i > 10)
+            println("Vector 1")
+        i = 1
+        do {
+            println(array[i])
+            i = i + 1
+        } while (i > 10)
+            println("Mayor, $ma,posicion, $i")
+            println("Menor, $me,posicion, $i")
+    }
--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
@@ -1,6 +1,2 @@
 package com.example.kotlinprogramacionestructurada
 
-fun calcularSuma(a: Int, b: Int): Int {
-    val suma = a + b
-    return suma
-}
