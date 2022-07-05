54cec92c158856bb8d34b26d90bcf812fbf6d7a7
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-05 04:51:02 -0500

    Ejercicio 55:programa que capture con número enteros una matriz de 10 X 10 y al final de cada renglón imprima la suma de sus elementos

--- src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
+++ src/main/kotlin/com/example/kotlinprogramacionestructurada/Utilitarios.kt
@@ -1,6 +1,29 @@
 package com.example.kotlinprogramacionestructurada
 
-fun calcularSuma(a: Int, b: Int): Int {
-    val suma = a + b
-    return suma
+fun calcularSumaDeUnVectorPorFila() {
+    var s = 0
+    var numeroDeFilas = 3
+    var numeroDeColumnas = 3
+    val matriz = arrayOf(
+        intArrayOf(-1, -1, -1),
+        intArrayOf(-1, -1, -1),
+        intArrayOf(-1, -1, -1)
+    )
+    for (i in 0..numeroDeFilas - 1) {
+        for (j in 0..numeroDeColumnas - 1) {
+            println("dame dato: $i,$j")
+            matriz[i][j] = readLine()!!.toInt()
+        }
+    }
+    for (i in 0..numeroDeFilas - 1) {
+        s = 0
+        for (j in 0..numeroDeColumnas - 1) {
+            s += matriz[i][j]
+        }
+        for (j in 0..numeroDeColumnas - 1) {
+            println(matriz[i][j])
+        }
+        println("la suma de este reglon es:, $s")
+        println("SALTO DE LINEA")
+    }
 }
